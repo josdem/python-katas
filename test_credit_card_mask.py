2817aa344030debda1d11447960a384c6b4c7a83
josdem <joseluis.delacruz@gmail.com>
2021-07-04 16:13:52 -0400

    Adding credit card mask test fail

--- test_credit_card_mask.py
+++ test_credit_card_mask.py
@@ -7,6 +7,8 @@ class FixedTest(unittest.TestCase):
 
     def tests(self):
         self.assertEqual(maskify(""), "")
+        self.assertEqual(maskify("123"), "123")
+        self.assertEqual(maskify("SF$SDfgsd2eA"), "########d2eA")
 
 if __name__ == '__main__':
     unittest.main()
