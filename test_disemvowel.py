c56bdbd4d145627ad8e181385094985861233e5b
josdem <joseluis.delacruz@gmail.com>
2021-07-06 19:57:53 -0400

    Adding kata instructions

--- test_disemvowel.py
+++ test_disemvowel.py
@@ -1,5 +1,13 @@
 import unittest
 
+'''
+Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: for this kata y isn't considered a vowel.
+'''
+
 def disemvowel(string):
     array = list(string)
     result = filter(lambda ch: ch != 'a' and ch != 'e' and ch != 'i' and ch != 'o' and ch != 'u' and ch != 'A'
