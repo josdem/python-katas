5e0347805c06c6be0ffacd105e936245df43a89c
josdem <joseluis.delacruz@gmail.com>
2021-07-07 08:44:58 -0400

    Adding array diff kata

---
+++ test_array_diff.py
@@ -0,0 +1,18 @@
+import unittest
+
+'''
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b keeping their order.
+'''
+
+def array_diff(a, b):
+    return a
+
+class FixedTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(array_diff([1,2],[1]), [2])
+        self.assertEqual(array_diff([1,2,2,2,3],[2]), [1,3])
+
+if __name__ == '__main__':
+    unittest.main()
