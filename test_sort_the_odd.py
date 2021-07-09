7d5a71ac614249063337cac8a3c87674ec35b862
josdem <joseluis.delacruz@gmail.com>
2021-07-09 07:54:53 -0400

    Adding sort the odds kata

---
+++ test_sort_the_odd.py
@@ -0,0 +1,18 @@
+import unittest
+
+'''
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+'''
+
+def sort_array(array):
+    return array
+
+class FixedTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+        self.assertEqual(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+        self.assertEqual(sort_array([]), [])
+
+if __name__ == '__main__':
+    unittest.main()
