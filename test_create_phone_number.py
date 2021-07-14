d006d17169b54bf8b84a0f56fd00cd9bfc5aab61
josdem <joseluis.delacruz@gmail.com>
2021-07-14 07:45:33 -0400

    Adding create phone number kata

---
+++ test_create_phone_number.py
@@ -0,0 +1,20 @@
+import unittest
+
+'''
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+'''
+
+def create_phone_number(array):
+    return ""
+
+class FixedTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+        self.assertEqual(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111")
+        self.assertEqual(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+        self.assertEqual(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]), "(023) 056-0890")
+        self.assertEqual(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), "(000) 000-0000")
+
+if __name__ == '__main__':
+    unittest.main()
