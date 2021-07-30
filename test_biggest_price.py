e7a3dceac9689a71cbdf4267a74af1ac7ff4d7e1
josdem <joseluis.delacruz@gmail.com>
2021-07-30 08:33:52 -0400

    Adding biggest price kata

---
+++ test_biggest_price.py
@@ -0,0 +1,23 @@
+import unittest
+
+'''
+Messages with random data are coming! But we just care about prices!
+Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
+messages = ["hi", "2.0", "@#$%", "32.0"]
+result = 32.0
+'''
+
+def biggest(data):
+    return len(data)
+
+class FixedTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(biggest(['hi', '2.0', '@#$%', '32.0']), 32.0)
+        self.assertEqual(biggest(['pair', 'car', '-10.0']), -10.0)
+        self.assertEqual(biggest(['main', '3.14', 'yaml']), 3.14)
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
