9b6607362aadb03877330f5c8864010ffd8330cb
josdem <joseluis.delacruz@gmail.com>
2021-08-14 08:23:34 -0400

    Adding birthday cake candles kata

---
+++ test_birthday_cake_candles.py
@@ -0,0 +1,19 @@
+import unittest
+
+'''
+It is your birthday! And you want to make it special, so you want to keep only biggest candles in the cake
+Your task is to create a function that removes all small candles and just keep the biggest ones.
+'''
+
+def counter(candles):
+    return candles
+
+class FixedTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(counter([]), [])
+        self.assertEqual(counter([1]), [1])
+        self.assertEqual(counter([3, 2, 1, 3]), [3, 3])
+
+if __name__ == '__main__' : 
+    unittest.main()
