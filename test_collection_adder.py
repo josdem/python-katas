49b75631a17fd9658d076b7409bc67e7385eaa65
josdem <joseluis.delacruz@gmail.com>
2021-08-15 10:10:31 -0400

    Adding sum a collection kata solution

--- test_collection_adder.py
+++ test_collection_adder.py
@@ -4,14 +4,14 @@ import unittest
 Given an integer collection return a sum of its elements
 '''
 
-def sum(numbers):
-    return len(numbers)  
+def adder(numbers):
+    return sum(numbers)  
 
 class FixedTest(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(sum([5, 4, 1, 2, 9]), 21)
-        self.assertEqual(sum([1, 2, 3, 4, 10, 11]), 31)
+        self.assertEqual(adder([5, 4, 1, 2, 9]), 21)
+        self.assertEqual(adder([1, 2, 3, 4, 10, 11]), 31)
 
 if __name__ == '__main__':
     unittest.main()
