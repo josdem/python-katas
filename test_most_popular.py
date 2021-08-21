d94a52fdcb91eeeea0a72513222fad6106e1a3f8
josdem <joseluis.delacruz@gmail.com>
2021-08-21 09:42:44 -0400

    Deleting unused line

--- test_most_popular.py
+++ test_most_popular.py
@@ -4,7 +4,6 @@ import itertools
 '''
 Given a collection with numbers: [31 , 34, 56, 34, 12, 35, 24, 34, 69, 18]
 Write a function that finds most popular number in the array, in this case 34 because it is the number that appears most often.
- */
 '''
 
 def find(numbers):
