bd60049df5e89662620a68cbfa5fefccecc06cb9
josdem <joseluis.delacruz@gmail.com>
2021-08-21 09:01:26 -0400

    Adding brute force most popular solution

--- test_most_popular.py
+++ test_most_popular.py
@@ -1,11 +1,19 @@
 import unittest
+import itertools
 
 def find(numbers):
-    return len(numbers)
+    max = 0
+    bigger = numbers[0]
+    for n in numbers:
+        count = numbers.count(n)
+        if (count > max):
+            max = count
+            bigger = n
+    return bigger
 
 class FixedTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(31, find([31 , 34, 34, 56, 12, 35, 24, 34, 69, 18]))
+        self.assertEqual(34, find([31 , 34, 34, 56, 12, 35, 24, 34, 69, 18]))
 
 if __name__ == '__main__':
     unittest.main()
