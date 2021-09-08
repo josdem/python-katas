b3f1cbddfb102da0a21e581c9a52061b7f40b610
josdem <joseluis.delacruz@gmail.com>
2021-09-07 19:07:56 -0500

    Adding counting numbers kata solution

--- test_counting_numbers.py
+++ test_counting_numbers.py
@@ -1,8 +1,14 @@
 import unittest
 import unittest
 
+'''
+Given an integer collection, return an array with three elements:
+how many of them are positive, how many negative and how many are zeros.
+Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
+'''
+
 def counter(numbers):
-    return numbers
+    return [len([x for x in numbers if x > 0]), len([x for x in numbers if x < 0]), len([x for x in numbers if x == 0])]
 
 class FixedTest(unittest.TestCase):
     def test(self):
