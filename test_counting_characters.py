ae55facfedb848c92be83a9ed2d4a327948197e3
josdem <joseluis.delacruz@gmail.com>
2021-12-24 13:36:50 -0600

    Adding counting characters test

---
+++ test_counting_characters.py
@@ -0,0 +1,17 @@
+import unittest
+'''
+ Counting vowels and consonants. Create two functions one for counting vowels and another for counting consonants
+ Given: A string
+ When: That string is josdem
+ Then: Counting vowels should be 2 and consonants 4
+'''
+
+def countVowels(keyword):
+    return len(keyword)
+
+class FixedTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(2, countVowels("josdem"))
+
+if __name__ == '__main__':
+    unittest.main()
