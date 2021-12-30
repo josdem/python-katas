c370d2416edb9c88cbdb1b07a58876f36d8eaf9e
josdem <joseluis.delacruz@gmail.com>
2021-12-30 10:58:25 -0600

    Adding string compressor kata

---
+++ test_string_compressor.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.case import expectedFailure
+
+def compress(keyword):
+    return keyword
+
+class FixedTest(unittest.TestCase):
+    def test(self):
+        keyword = "aaabbbbcc"
+        expectedString = "a3b4c2"
+        self.assertEqual(compress(keyword), expectedString)
+
+if __name__ == '__main__':
+    unittest.main()
