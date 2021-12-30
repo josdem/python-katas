9078f41ecfd4067894a4354524ce327c9adf8961
josdem <joseluis.delacruz@gmail.com>
2021-12-30 12:30:19 -0600

    Splitting common elements in two files

---
+++ common_elements.py
@@ -0,0 +1,2 @@
+def find(first, second):
+    return [n for n in first if n in second]
--- test_common_elements.py
+++ test_common_elements.py
@@ -1,12 +1,10 @@
 import unittest
+from common_elements import *
 
 '''
 I have two arrays with integers. I want to return elements in common.
 Given: [1,2,3,4,5] and [1,3,5,7,9] then expected output: [1,3,5]
 '''
-
-def find(first, second):
-    return [n for n in first if n in second]
 
 class FixedTest(unittest.TestCase):
     def test(self):
