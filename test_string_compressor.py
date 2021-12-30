94fb8be977bf88e958216d1a203c717c4b605519
josdem <joseluis.delacruz@gmail.com>
2021-12-30 12:01:44 -0600

    Splitting string compressor in two files

--- test_string_compressor.py
+++ test_string_compressor.py
@@ -1,17 +1,11 @@
 import unittest
-import itertools
+from string_compressor import *
 
 '''
 Given a string "aaabbbbcc"
 When we call compress method
 Then we have a compressed string like "a3b4c2"
 '''
-
-def compress(keyword):
-    result = ""
-    for k,v in itertools.groupby(keyword, lambda n: keyword.count(n)):
-        result = result + list(v)[0] + str(k)
-    return result
 
 class FixedTest(unittest.TestCase):
     def test(self):
