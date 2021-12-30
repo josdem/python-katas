61b4db3ffc729355f7aefec0417860efa7a767e7
josdem <joseluis.delacruz@gmail.com>
2021-12-30 12:32:31 -0600

    Splitting counting numbers in two files

--- test_counting_numbers.py
+++ test_counting_numbers.py
@@ -1,13 +1,11 @@
 import unittest
+from counting_numbers import *
 
 '''
 Given an integer collection, return an array with three elements:
 how many of them are positive, how many negative and how many are zeros.
 Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
 '''
-
-def counter(numbers):
-    return [len([x for x in numbers if x > 0]), len([x for x in numbers if x < 0]), len([x for x in numbers if x == 0])]
 
 class FixedTest(unittest.TestCase):
     def test(self):
