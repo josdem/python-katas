1fb7f2832603a827ec6162550a9ecee6f826ab21
josdem <joseluis.delacruz@gmail.com>
2021-12-30 12:31:25 -0600

    Splitting counting characters in two files

--- test_counting_characters.py
+++ test_counting_characters.py
@@ -1,20 +1,11 @@
 import unittest
+from counting_characters import *
 '''
  Counting vowels and consonants. Create two functions one for counting vowels and another for counting consonants
  Given: A string
  When: That string is josdem
  Then: Counting vowels should be 2 and consonants 4
 '''
-
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'r', 'p', 'q', 's', 't', 'v', 'w','x', 'y', 'z']
-
-def countVowels(keyword):
-    return len([x for x in keyword if x in vowels])
-
-def countConsonants(keyword):
-    return len([x for x in keyword if x in consonants])
-   
 
 class FixedTest(unittest.TestCase):
     def test(self):
