58898c9bbd3a04f6370e547c75924a8673ec780c
josdem <joseluis.delacruz@gmail.com>
2021-12-31 15:40:21 -0600

    Deleting disem vowel and find the odd solution

--- python/disem_vowel.py
+++ python/disem_vowel.py
@@ -1,5 +1,2 @@
 def disemvowel(string):
-    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    array = list(string)
-    result = [ch for ch in array if ch not in vowels]
-    return ''.join(result)
+    return string
