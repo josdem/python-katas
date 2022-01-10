06d15c0705db867afd8240a85e14a59494c81c98
josdem <joseluis.delacruz@gmail.com>
2022-01-10 16:02:55 -0600

    Adding pairs to the list

--- python/shop_calculator.py
+++ python/shop_calculator.py
@@ -1,2 +1,7 @@
 def compute(amount, keyboards, usbs):
+    pairs = []
+    for k in keyboards:
+        for u in usbs:
+            pairs.append((k, u))
+    print(*pairs)        
     return amount + len(keyboards) + len(usbs)
