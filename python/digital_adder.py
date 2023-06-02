95a5a3b50dc01e0ccb5921bcb4401067758ae708
josdem <joseluis.delacruz@gmail.com>
2023-06-02 15:22:31 -0400

    Adding digital numbers solution

--- python/digital_adder.py
+++ python/digital_adder.py
@@ -1,2 +1,6 @@
 def adder(numbers):
-    return numbers
+    result = []
+    numbersAsString = [str(n) for n in numbers]
+    for item in numbersAsString:
+        result.append(sum([int(n) for n in list(item)]))
+    return result
