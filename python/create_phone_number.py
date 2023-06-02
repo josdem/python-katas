2aa3c488db188eca381922a4c6caa74a632aaac1
josdem <joseluis.delacruz@gmail.com>
2023-06-02 15:07:38 -0400

    Delete create phone number solution

--- python/create_phone_number.py
+++ python/create_phone_number.py
@@ -1,6 +1,2 @@
-from python.array_diff import array_diff
-
-
 def create_phone_number(array):
-    string = ''.join(str(n) for n in array)
-    return f'({string[:3]}) {string[3:6]}-{string[6:]}'
+    return str(array)
