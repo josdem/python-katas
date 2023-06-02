08138cbad9ce197854d13d23d9323849b53d2bff
josdem <joseluis.delacruz@gmail.com>
2023-06-02 15:57:25 -0400

    Deleting most popular solution

--- python/most_popular.py
+++ python/most_popular.py
@@ -1,10 +1,2 @@
-import itertools
-
 def find(numbers):
-    max = 0
-    bigger = numbers[0]
-    for k, v in itertools.groupby(numbers, lambda n: numbers.count(n)):
-        if(k > max):
-            max = k
-            bigger = list(v)[0]
-    return bigger
+    return len(numbers)
