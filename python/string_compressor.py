939ceeebb14b5c1d79854611fed981e33467ee65
josdem <joseluis.delacruz@gmail.com>
2023-06-02 16:25:13 -0400

    Deleting strting compressor solution

--- python/string_compressor.py
+++ python/string_compressor.py
@@ -1,7 +1,2 @@
-import itertools
-
 def compress(keyword):
-    result = ""
-    for k,v in itertools.groupby(keyword, lambda n: keyword.count(n)):
-        result = result + list(v)[0] + str(k)
-    return result
+    return keyword
