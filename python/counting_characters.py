274257476bca1b4c8c8ad8802c37451fd90a2360
josdem <joseluis.delacruz@gmail.com>
2023-06-02 15:00:48 -0400

    Deleting counting characters solution

--- python/counting_characters.py
+++ python/counting_characters.py
@@ -1,8 +1,5 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'r', 'p', 'q', 's', 't', 'v', 'w','x', 'y', 'z']
-
 def countVowels(keyword):
-    return len([x for x in keyword if x in vowels])
+    return len(keyword)
 
 def countConsonants(keyword):
-    return len([x for x in keyword if x in consonants])
+    return len(keyword)
