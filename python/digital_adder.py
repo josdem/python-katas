2a268df45089c79e35a67eb9bffaf837fce1d49f
josdem <joseluis.delacruz@gmail.com>
2023-06-02 15:21:42 -0400

    Deleting digital numbers solution

--- python/digital_adder.py
+++ python/digital_adder.py
@@ -1,9 +1,2 @@
-from unittest import result
-
-
 def adder(numbers):
-    result = []
-    numbersAsString = [str(n) for n in numbers]
-    for item in numbersAsString:
-        result.append(sum([int(n) for n in list(item)]))
-    return result
+    return numbers
