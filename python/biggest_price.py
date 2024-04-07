a4f45d38f903d41dd00e888d4395b93893e16e19
josdem <joseluis.delacruz@gmail.com>
2024-04-07 15:17:22 -0400

    Improving implementation

--- python/biggest_price.py
+++ python/biggest_price.py
@@ -3,4 +3,4 @@ import re
 pattern = '-?[0-9]+.?[0-9]+'
 
 def biggest(data):
-    return float(max([l for l in data if re.match(pattern, l)]))
+    return float(max([n for n in data if re.match(pattern, n)]))
