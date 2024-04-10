00e62a07c31d77bab51e9578b5f6cff2f010c618
josdem <joseluis.delacruz@gmail.com>
2024-04-10 13:38:47 -0400

    Adding test description

--- test_counting_character.py
+++ test_counting_character.py
@@ -1,6 +1,12 @@
 import unittest
 from python.counting_character import count
 
+'''
+Given: A string
+When: We call count character method
+Then: I count how many times this character appears in the string
+'''
+
 class FixedTest(unittest.TestCase):
     def test(self):
         self.assertEqual(1, count("josdem", 'm'))
