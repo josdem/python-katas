4e0bf55ac27606dec0686ab8238dd13f42eb46f4
josdem <joseluis.delacruz@gmail.com>
2024-04-12 19:32:02 -0400

    Adding filter username solution

--- python/filter_username.py
+++ python/filter_username.py
@@ -1,2 +1,6 @@
 def filter_username(username):
-    return len(username)
+    uppercase = 0
+    for char in username:
+        if char.isupper():
+            uppercase += 1
+    return uppercase / len(username) < 0.5
